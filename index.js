b154a54b791c4a5ce51bbd6ce4c1e9767aa8bfa0
dhseo <dhseo@antock.com>
2021-01-31 19:15:23 +0900

    1. graphql movie example completed

---
+++ index.js
@@ -0,0 +1,8 @@
+const { GraphQLServer } = require("graphql-yoga");
+import resolvers from "./graphql/resolvers";
+const server = new GraphQLServer({
+    typeDefs: "graphql/schema.graphql",
+    resolvers,
+});
+
+server.start( () => console.log("Graphql Server Running"))
